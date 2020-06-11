7b052d10b2289368001363373b6d964ccc72dec4
= <=>
2020-06-11 22:50:13 +0000

    C project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,3 @@
-
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-puts("Programming is like building a multilingual puzzle\n");
+#include <stdio.h>                                                                                                                                                  /**                                                                                * main - Entry point                                                              *                                                                                 * Return: Always 0 (Success)                                                      */                                                                               int main(void)                                                                    {                                                                                 puts("Programming is like building a multilingual puzzle\n"); 
 return (0);
-}
-
+} 
