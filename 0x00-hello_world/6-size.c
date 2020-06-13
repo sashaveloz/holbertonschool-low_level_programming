e36a95230fa5d2ea73fcd8e0380ae74e57cc292b
= <=>
2020-06-13 23:13:08 +0000

    C project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.c>
+/* 
+   *main-Entry point
+   *printing sizesk
+   *Return: Always 0 (Succes)
+   */
+int main(void)
+{
+	/*Variables declaration */
+
+	char charType;
+	int intType;
+	long longType;
+	long long int longlongintType
+	float floatType;
+	printf ("Size of a char: %ld byte(s)\n" sizeof(charType));
+	printf ("Size of an int: %ld byte(s)\n" sizeof(intType))
+	printf ("Size of a long int %ld byte(s)\n" sizeof(longType));
+	printf ("Size of a long long int %ld byte(s)\n" sizeof(longlongType));
+	printf ("Size of a float %ld byte(s)\n" sizeof(floatType))
+	
+	return(0);
+}
