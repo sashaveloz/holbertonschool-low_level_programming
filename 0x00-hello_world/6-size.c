27556d4128a4198297a3d04205a59b5607004ed6
= <=>
2020-06-14 21:32:28 +0000

    C project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,12 @@
    *Return: Always 0 (Succes)
    */
 	int main(void);
-{
+{	char charType;
+	int intType;
+	long longType;
+	long long int longlongintType;
+	float floatType;
+
 	printf("Size of a char: %ld byte(s)\n" sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n" sizeof(intType));
 	printf("Size of a long int %ld byte(s)\n" sizeof(longType));
