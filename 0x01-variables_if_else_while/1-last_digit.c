0114176ec1eff605ecd57a49fa48b2ceaa77f49e
= <=>
2020-06-15 22:34:52 +0000

    variables c project

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	int lastdig;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastdig = n % 10;
+	if (lastdig > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastdig);
+	}
+	else if (lastdig == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastdig);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdig);
+	}
+	return (0);
+}
