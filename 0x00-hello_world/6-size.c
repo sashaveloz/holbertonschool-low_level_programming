1c56f20cca978ea9e25b63ff1334835dca8d2590
= <=>
2020-06-15 02:59:08 +0000

    c project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,8 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
 	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
-	printf("Size of a long int %ld byte(s)\n", sizeof(longType));
-	printf("Size of a long long int %ld byte(s)\n", sizeof(longlongintType));
-	printf("Size of a float %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 	return (0);
 }
