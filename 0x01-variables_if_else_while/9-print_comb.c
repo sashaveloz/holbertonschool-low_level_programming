0f079ec742400b86a64db0ba0fcac6bc2dc010f6
= <=>
2020-06-16 01:30:36 +0000

    2 c project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int n;
+
+	n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+		putchar(',');
+		putchar(' ');
+	}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
