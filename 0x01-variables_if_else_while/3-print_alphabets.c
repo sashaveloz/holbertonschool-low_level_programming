5d583dd262b810851f4263d04f32c12c10255797
= <=>
2020-06-16 00:19:30 +0000

    c 2 project

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char a;
+	char A;
+
+	a = 'a';
+	A = 'A';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
