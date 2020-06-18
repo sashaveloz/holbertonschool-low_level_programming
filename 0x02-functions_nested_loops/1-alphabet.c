92dd37f8e0cfd5f50acd783daf37095bdcf761c1
= <=>
2020-06-18 03:30:34 +0000

    3 c project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_alphabet - Entry point
+ *Print the alphabet
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
