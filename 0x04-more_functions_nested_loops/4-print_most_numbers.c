86e6927ea7f7bc79e5f60df90aee52278198b654
= <=>
2020-06-21 01:27:01 +0000

    4 c project

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+  * print_most_numbers - Entry point
+  *
+  *Return: 0
+  *
+  *
+  */
+
+void print_most_numbers(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
