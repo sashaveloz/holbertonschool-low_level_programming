eae9789d2b87431075351965bccec98f2aaf1b5a
= <=>
2020-06-21 04:27:41 +0000

    3 c project

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - cause an infinite loop
  * Return: 0
  */
-
 int main(void)
 {
 int i;
@@ -13,12 +12,13 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
- * Commenting loop
+ * Commenting - loop
  * while (i < 10)
  *  {
  * putchar(i);
  *   }
  */
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,14 +6,3 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
-
-_putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-int add(int, int);
-void print_to_98(int n);
