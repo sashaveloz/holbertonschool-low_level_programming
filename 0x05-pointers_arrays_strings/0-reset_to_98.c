0eb1b5821155c755c3f73f9d384d4f281613554f
= <=>
2020-06-23 14:44:28 +0000

    5 c project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+  * reset_to_98 - Entry point
+  * A pointer - that takes an int as parameter and updates the value
+  *it points to 98
+  *
+  *@n: pointer
+  *Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
