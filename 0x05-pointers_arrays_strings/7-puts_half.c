d69f73b1a84f4f30478fc4d19f247c361be4b1ce
= <=>
2020-06-24 18:44:22 +0000

    5 c project

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+  *puts_half - Print a half of a string
+  *
+  *Return: nothing
+  *@str: char *
+  */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int n;
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	if (i % 2 == 1)
+	{
+		n = (i - 1) / 2;
+	}
+	else
+	{
+		n = i / 2;
+	}
+	for (; str[n]; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
