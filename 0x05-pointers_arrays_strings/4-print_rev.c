55a73ead27225332e1318ddf16ada37ec3566ffb
= <=>
2020-06-24 04:23:24 +0000

    5 c project

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+  * print_rev - Entry point
+  * Printing reverse string
+  * @s: string
+  *
+  */
+
+void print_rev(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+
+	for (a -= 1; s[a] >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+
+}
