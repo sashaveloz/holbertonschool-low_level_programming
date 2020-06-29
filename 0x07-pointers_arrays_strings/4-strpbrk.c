512a52bdb252b21d827e29feb7538bee5ee722e8
= <=>
2020-06-29 06:32:27 +0000

    function that searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+  * _strpbrk - searches a string for any set of bytes
+  *
+  *@s: string *
+  *@accept: string *
+  *
+  *Return: a * to the byte in s that matches one of the bytes in
+  *accept or NULL if no such file is found.
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
