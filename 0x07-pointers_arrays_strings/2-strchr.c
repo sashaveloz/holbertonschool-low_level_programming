adf4c82f08dba25ab6562f1edf7994bd6c7fe1ef
= <=>
2020-06-29 05:00:46 +0000

    function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
   *_strchr - Function that locates a char in a string
@@ -23,5 +22,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return ('\0');
+	return (NULL);
 }
