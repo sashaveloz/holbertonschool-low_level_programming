2eba66dd1b6990763fb1d4c2bff470a147261ec9
= <=>
2020-06-29 02:23:48 +0000

    Function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+  * *_memset - function that fills memory with a constant byte
+  *@s: string
+  *@b: byte
+  *@n: number of members
+  * Return: string
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
