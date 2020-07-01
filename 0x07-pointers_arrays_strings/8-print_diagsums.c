08c9924b8f9afb82862faf120f72dfc13d9122de
= <=>
2020-07-01 03:09:14 +0000

    Sum of two diagonals of a square matrix of integers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+  *print_diagsums - Prints the sum of two diagonals of a square
+  * matrix of integers
+  *
+  *@a: int *
+  *@size: int *
+  *
+  *Return: void
+  */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int uno = 0, dos = 0;
+
+	for (i = 0; i < (size * size); i += size + 1)
+	{
+		uno += a[i];
+	}
+	for (i -= size * 2; i >= 0; i -= size - 1)
+	{
+		dos += a[i];
+	}
+	printf("%d, %d\n", uno, dos);
+}
