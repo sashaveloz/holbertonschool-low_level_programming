ffbcc1fe28080ad2a53588f282edf1a0be86fa91
= <=>
2020-07-02 22:34:05 +0000

    returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  * _strlen_recursion - Function that returns the length of a string
+  *
+  *@s: string *
+  *
+  * Return: integer
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	return (_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
