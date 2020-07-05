5ddf11ab59c21611f896ab7df31e3d00ffaeb92b
= <=>
2020-07-05 03:47:05 +0000

    argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+  * main - Multiplies two numbers
+  *@argc: int
+  *@argv: *
+  *Return: -1
+  */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+
+	b = atoi(argv[1]);
+	a = atoi(argv[2]);
+	
+	printf("%d\n", a*b);
+
+	return (0);
+}
