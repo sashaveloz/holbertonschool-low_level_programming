eb9a07bffbcbe10e622c23b926008edeb3ceb27c
= <=>
2020-07-05 02:33:04 +0000

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+  *main - Entry point
+  *@argc: num of comm line argument
+  *@argv: program  line argumentr
+  *Return: 0
+  */
+
+int main (int argc, char * argv[])
+{
+	(void)argc;
+
+	printf("%s", argv[0]);
+	
+	return (0);
+}
