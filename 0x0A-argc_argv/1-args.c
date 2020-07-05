a69af866dbacfb57847da3176c16c9f7ff9b2577
= <=>
2020-07-05 02:48:49 +0000

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+  *main - print the number of arguments
+  *
+  *@argv: *
+  *@argc: int
+  *
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
