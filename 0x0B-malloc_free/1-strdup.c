a6d2eda5101ed8ab217096b9f52c119010b4aae4
= <=>
2020-07-08 02:13:54 +0000

    Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+  *_strdup - returns a pointer to a new string which is a duplicate of
+  * the string.
+  *@str: * string
+  *Return: NULL or String
+  */
+char *_strdup(char *str)
+{
+	char *s;
+	unsigned int a;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+		;
+	s = malloc(sizeof(char) * a);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		s[a] = str[a];
+	}
+	s[a] = '\0';
+
+	return (s);
+}
