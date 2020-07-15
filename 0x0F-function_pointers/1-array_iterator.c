eb9613d4a56cfe2d2c2b5363ee7a6cd65aead571
= <=>
2020-07-15 21:51:59 +0000

    Function that executes a function given as a parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+  *array_iterator - function that executes a function given as a
+  *parameter on each element of an array.
+  *@array: array of elements
+  *@size: size of array
+  * @action: pointer to function
+  *Return: void
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
