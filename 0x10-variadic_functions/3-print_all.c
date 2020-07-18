91f5f91cfbdbbfff099e76f25ed15a45bb497f29
= <=>
2020-07-18 22:58:46 +0000

    uploads

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,89 @@
+#include "variadic_functions.h"
+
+/**
+  *print_all - function that prints anything.
+  *@format: list of types of arguments passed to
+  *the function
+  *Return: void
+  */
+
+void print_all(const char * const format, ...)
+{
+	va_list i;
+	int j, m;
+	char *separator = "";
+
+	op_t a[] = {{"s", print_string},
+		{"c", print_char},
+		{"f", print_float},
+		{"i", print_integer},
+		{NULL, NULL}};
+
+	va_start(i, format);
+
+	j = 0;
+	while (format && format[j])
+	{
+		m = 0;
+		while (a[m].op)
+		{
+			if (a[m].op[0] == format[j])
+			{
+				printf("%s", separator);
+				a[m].f(i);
+				separator = ", ";
+			}
+			m++;
+		}
+		j++;
+	}
+	printf("\n");
+	va_end(i);
+}
+
+/**
+  *print_char - function that prints a char
+  *@c: char
+  *Return: void
+  */
+
+void print_char(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+  *print_string - functiont that prints a string
+  *@s: string
+  *Return: void
+  */
+void print_string(va_list s)
+{
+	char *c;
+
+	c = va_arg(s, char *);
+	if (c)
+		printf("%s", c);
+	else
+		printf("(nil)");
+}
+
+/**
+  *print_integer - function that prints an integer
+  *@i: int
+  *Return: void
+  */
+void print_integer(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+  *print_float - function that prints a float
+  *@f: double
+  *Return: void
+  */
+void print_float(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,10 +5,27 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+  *struct
+  */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+  *functions to print
+  */
+void print_char(va_list c);
+void print_string(va_list s);
+void print_integer(va_list i);
+void print_float(va_list f);
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H	*/
