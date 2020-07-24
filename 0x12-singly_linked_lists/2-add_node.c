774b128b9471461d98b24a3b1a04de84b20738f4
sashaveloz <2065@holbertonschool.com>
2020-07-24 23:51:58 +0000

    Function that adds a node at the beggining

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+  *add_node - function that adds a node at the beggining of a list
+  *@head: head of linked list
+  *@str: element string of linked list
+  *Return: the address of the new element, or NULL if it failed
+  */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	char *str2;
+	unsigned int lenght = 0;
+
+	if (!str)
+		return (NULL);
+
+	node = malloc(sizeof(list_t));
+
+	if (!node)
+		return (NULL);
+
+	str2 = strdup(str);
+
+	while (str2[lenght])
+		lenght++;
+
+	node->str = str2;
+	node->len = lenght;
+
+	if (!head || !(*head))
+		node->next = NULL;
+
+	else
+		node->next = *head;
+		*head = node;
+
+	return (node);
+}
