df2416915358906ab8ac88b5f0b82b83c003ad7b
sashaveloz <2065@holbertonschool.com>
2020-07-28 22:39:20 +0000

    Function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+  *free_listint2 - function that frees a listint_t list
+  *@head: element of list
+  */
+
+void free_listint2(listint_t **head)
+{
+	if (head && *head)
+	{
+		free_listint2(&(*(*head)).next);
+		free(*head);
+	}
+	*head = NULL;
+}
