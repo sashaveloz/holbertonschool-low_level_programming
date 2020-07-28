581c200553e7f7d39460484a84d72454e38d361a
sashaveloz <2065@holbertonschool.com>
2020-07-28 19:10:30 +0000

    Function that returns the number of elements of a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,19 @@
 #include "lists.h"
 
 /**
-  *listint_len - function that returns the number of elements in list
-  *@h: structure elements
-  *Return: n of elemens
-  */
+ *listint_len - function that returns the number of elements in list
+ *@h: structure elements
+ *Return: n of elemens
+ */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t elem;
+	size_t n_nodes = 0;
 
-	elem = h->n;
-
-	return (elem);
+	while (h)
+	{
+		n_nodes++;
+		h = h->next;
+	}
+	return (n_nodes);
 }
