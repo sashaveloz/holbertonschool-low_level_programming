84e53c0d2d4bbc70593682bfc42ed54cd0096fac
sashaveloz <2065@holbertonschool.com>
2020-07-29 00:52:20 +0000

    Function that delets a node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+  *delete_nodeint_at_index - deletes a node
+  *@head: element of list
+  *@index: index of list
+  *Return: int
+  */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr_h, *tmp;
+	unsigned int idx;
+
+	if (!*head)
+		return (-1);
+
+	ptr_h = *head;
+
+	if (index == 0)
+	{
+		*head = ptr_h->next;
+		free(ptr_h);
+		return (1);
+	}
+	while (ptr_h && idx < (index - 1))
+	{
+		idx++;
+		ptr_h = ptr_h->next;
+	}
+	if (!ptr_h || ptr_h->next)
+		return (-1);
+
+	tmp = ptr_h->next->next;
+
+	free(ptr_h->next);
+
+	ptr_h = tmp;
+
+	return (1);
+}
